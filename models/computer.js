7b56204a52ea8a042a47e8ca89f43a89f800ff5a
DerianCordobaPerez <derianunanleon@gmail.com>
2022-05-16 01:22:17 -0600

    feat: Separate run logic in computer model

--- models/computer.js
+++ models/computer.js
@@ -1,5 +1,6 @@
 import mongoose, { model, Schema } from 'mongoose'
 import { execute } from 'utils/commands'
+import { calculateTimeCallback } from 'utils/time'
 
 /* Creating a schema for the computer model. */
 const computerSchema = new Schema({
@@ -29,6 +30,25 @@ const computerSchema = new Schema({
  */
 export function isConnected({ ip }) {
   return execute({ command: `ping -c 1 -i 0.2 -w 1 ${ip}` })
+}
+
+/**
+ * It takes an IP address and a role, finds the computer with that IP address, and then runs the
+ * commands associated with that role on that computer
+ * @returns An array of objects with the name of the command and the output of the command.
+ */
+export function run({ ip, commands, role }) {
+  return [...commands, role].map((command) => {
+    const { output, time } = calculateTimeCallback(execute, {
+      command: `./monitoring.sh ${ip} ${command}`,
+    })
+
+    return {
+      name: command,
+      output,
+      time,
+    }
+  })
 }
 
 export default mongoose.models.Computer || model('Computer', computerSchema)
