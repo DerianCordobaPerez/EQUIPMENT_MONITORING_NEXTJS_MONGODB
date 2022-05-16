067026ab0dd4a84cad4f2ea5cd8b9f8130c71ef2
DerianCordobaPerez <derianunanleon@gmail.com>
2022-05-16 01:22:53 -0600

    refactor: Change class to className

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <Layout>
-      <h1 class="text-center title__home">Developers</h1>
+      <h1 className="text-center title__home">Developers</h1>
 
-      <div class="container__cards">
-        <div class="cardC shadow">
-          <div class="cardC__header">
+      <div className="container__cards">
+        <div className="cardC shadow">
+          <div className="cardC__header">
             <Image
               className="cardC__image"
               src="/img/Derian.jpg"
@@ -17,13 +17,13 @@ export default function Home() {
               width="70"
             />
 
-            <div class="header__title">
+            <div className="header__title">
               <h5>Derian Córdoba</h5>
               <span>Back-End Software Developer</span>
             </div>
-            <i class="bi bi-quote"></i>
+            <i className="bi bi-quote"></i>
           </div>
-          <div class="cardC__body">
+          <div className="cardC__body">
             Software Developer, currently working with PHP (Vanilla, Laravel)
             and JavaScript (Vanilla, Node, React, NextJs...). Interested in
             BackEnd development, using technologies such as NodeJs, Laravel,
@@ -31,8 +31,8 @@ export default function Home() {
           </div>
         </div>
 
-        <div class="cardC shadow">
-          <div class="cardC__header">
+        <div className="cardC shadow">
+          <div className="cardC__header">
             <Image
               className="cardC__image"
               src="/img/Jonathan.jpg"
@@ -41,13 +41,13 @@ export default function Home() {
               width="70"
             />
 
-            <div class="header__title">
+            <div className="header__title">
               <h5>Jonathan Mendoza</h5>
               <span>Front-End Web Developer</span>
             </div>
-            <i class="bi bi-quote"></i>
+            <i className="bi bi-quote"></i>
           </div>
-          <div class="cardC__body">
+          <div className="cardC__body">
             I am a 20-year-old passionate about Front-End web development, a
             fifth-year student of Information Systems Engineering at the
             university UNAN - León.
