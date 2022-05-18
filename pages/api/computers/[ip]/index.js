2799ded4ec195e80668508e1fdf5d543e615bcee
DerianCordobaPerez <derianunanleon@gmail.com>
2022-05-17 23:16:17 -0600

    feat: Use computer hook

---
+++ pages/api/computers/[ip]/index.js
@@ -0,0 +1,73 @@
+import connect from 'libs/database'
+import Computer, { isConnected, run } from 'models/computer'
+
+export default async function handler(req, res) {
+  const {
+    query: { ip },
+    method,
+  } = req
+
+  await connect()
+
+  switch (method) {
+    case 'GET':
+      try {
+        const computer = await Computer.findOne({ ip })
+
+        if (!computer) {
+          res.status(404).json({ success: false })
+        }
+
+        const connected = isConnected({ ip })
+        const commands =
+          connected &&
+          run({
+            ip,
+            commands: computer.commands,
+            role: computer.role,
+          })
+
+        res.status(200).json({
+          success: true,
+          data: {
+            ...computer._doc,
+            commands,
+            connected,
+          },
+        })
+      } catch (err) {
+        res.status(400).json({ success: false })
+      }
+      break
+    case 'PUT':
+      try {
+        const computer = await Computer.findOneAndUpdate({ ip }, req.body, {
+          new: true,
+          runValidators: true,
+        })
+
+        if (!computer) {
+          res.status(404).json({ success: false })
+        }
+
+        res.status(200).json({ success: true, data: computer })
+      } catch (err) {
+        res.status(400).json({ success: false })
+      }
+      break
+    case 'DELETE':
+      try {
+        const computer = await Computer.findOneAndDelete({ ip })
+        if (!computer) {
+          res.status(404).json({ success: false })
+        }
+        res.status(200).json({ success: true, data: {} })
+      } catch (err) {
+        res.status(400).json({ success: false })
+      }
+      break
+    default:
+      res.status(400).json({ success: false })
+      break
+  }
+}
--- pages/computers/[ip]/show.js
+++ pages/computers/[ip]/show.js
@@ -1,15 +1,16 @@
 import Layout from 'components/layout'
-import { API_COMPUTERS } from 'configs/env'
 import { useRouter } from 'next/router'
-import { isConnected, run } from 'models/computer'
 import { ToggleContext } from 'hooks/useToggle'
 import { useVanillaTilt } from 'hooks/useVanillaTilt'
 import { useContext } from 'react'
+import { useComputer } from 'hooks/useComputer'
+import Loading from 'components/loading'
 
-export default function Show({ computer, commands }) {
-  const { handleToggle, isToggleable } = useContext(ToggleContext)
+export default function Show() {
   const router = useRouter()
   const { ip } = router.query
+  const { data: computer, isLoading } = useComputer({ ip })
+  const { handleToggle, isToggleable } = useContext(ToggleContext)
   useVanillaTilt({ element: '.contentBox' })
 
   const handleDelete = async () => {
@@ -26,67 +27,53 @@ export default function Show({ computer, commands }) {
 
   return (
     <Layout>
-      <div className="d-flex align-items-center w-100 justify-content-between bg-light px-4 py-2 shadow rounded mb-4">
-        <h4>Monitoring {computer.name} pc</h4>
-
-        <button
-          disabled={!computer.connected}
-          className="btn btn-danger"
-          onClick={handleDelete}
-        >
-          Delete
-        </button>
-      </div>
-
-      {computer.connected ? (
-        commands
-          .filter((command) => command !== false)
-          .map(({ name, output, time }) => (
-            <pre key={name} className="p-2 shadow contentBox">
-              <div className="d-flex justify-content-between align-items-center">
-                <button
-                  className="btn btn-link text-white"
-                  onClick={() => handleToggle(name)}
-                >
-                  <i className="bi bi-caret-down-fill"></i>
-                </button>
-
-                <h5 className="text-decoration-underline">{name}</h5>
-
-                <span className="text-white fw-bold opacity-50">
-                  Execute time: {time}
-                </span>
-              </div>
-
-              {!isToggleable(name) && <code>{output}</code>}
-            </pre>
-          ))
+      {isLoading ? (
+        <Loading />
       ) : (
-        <pre className="p-2 shadow contentBox">
-          <h2 className="text-center">Computer Offline</h2>
-          <div className="separator__offline"></div>
-        </pre>
+        <>
+          <div className="d-flex align-items-center w-100 justify-content-between bg-light px-4 py-2 shadow rounded mb-4">
+            <h4>Monitoring {computer.name} pc</h4>
+
+            <button
+              disabled={!computer.connected}
+              className="btn btn-danger"
+              onClick={handleDelete}
+            >
+              Delete
+            </button>
+          </div>
+
+          {computer.connected ? (
+            computer.commands
+              .filter((command) => command !== false)
+              .map(({ name, output, time }) => (
+                <pre key={name} className="p-2 shadow contentBox">
+                  <div className="d-flex justify-content-between align-items-center bg__command">
+                    <button
+                      className="btn btn-link text-white"
+                      onClick={() => handleToggle(name)}
+                    >
+                      <i className="bi bi-caret-down-fill"></i>
+                    </button>
+
+                    <h5 className="text-decoration-underline">{name}</h5>
+
+                    <span className="text-white fw-bold opacity-50">
+                      Execute time: {time}
+                    </span>
+                  </div>
+
+                  {!isToggleable(name) && <code>{output}</code>}
+                </pre>
+              ))
+          ) : (
+            <pre className="p-2 shadow contentBox">
+              <h2 className="text-center">Computer Offline</h2>
+              <div className="separator__offline"></div>
+            </pre>
+          )}
+        </>
       )}
     </Layout>
   )
-}
-
-export async function getServerSideProps({ params }) {
-  const { ip } = params
-  const response = await fetch(`${API_COMPUTERS}/${ip}`)
-  const { data: computer } = await response.json()
-
-  const { role, commands } = computer
-
-  const connected = isConnected({ ip })
-
-  return {
-    props: {
-      computer: {
-        ...computer,
-        connected,
-      },
-      commands: connected && run({ ip, role, commands }),
-    },
-  }
 }
