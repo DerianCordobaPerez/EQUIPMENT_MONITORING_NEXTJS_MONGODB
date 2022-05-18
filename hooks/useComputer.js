064c4b0ecbcdbf003fb7304b74592ac14f93dc04
DerianCordobaPerez <derianunanleon@gmail.com>
2022-05-17 23:13:10 -0600

    feat: Add SWR hook with the computer managment

--- hooks/useComputer.js
+++ hooks/useComputer.js
@@ -1,10 +1,27 @@
 import useSWR from 'swr'
+import { useState, useEffect } from 'react'
 
+/**
+ * It fetches the computer data from the API and returns it, along with some metadata about the fetch
+ * @returns computer: data,
+ *   isLoading: !data && !error,
+ *   isError: error,
+ */
 export function useComputer({ ip }) {
-  const { data, error } = useSWR(`/api/computers/${ip}`)
+  const [autoRefresh, setAutoRefresh] = useState(false)
+
+  const { data, error } = useSWR(`/api/computers${ip ? `/${ip}` : ''}`, {
+    refreshInterval: autoRefresh ? 5000 : null,
+  })
+
+  useEffect(() => {
+    if (data) {
+      setAutoRefresh(true)
+    }
+  }, [data])
 
   return {
-    computer: data,
+    data,
     isLoading: !data && !error,
     isError: error,
   }
