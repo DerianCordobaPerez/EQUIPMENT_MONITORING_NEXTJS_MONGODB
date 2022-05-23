dd089c5ae558ed5aad8a362c035bc7353c2c4cd4
DerianCordobaPerez <derianunanleon@gmail.com>
2022-05-22 20:52:26 -0600

    fox: Solved select fill data

--- components/form/select.jsx
+++ components/form/select.jsx
@@ -1,19 +1,28 @@
 import ReactSelect from 'react-select'
+import { useRouter } from 'next/router'
 
 export default function Select({ field, change, value }) {
   const { uid, label, options, multiple } = field
+  const { pathname } = useRouter()
+
+  const isEdit = pathname.includes('edit')
 
   const onChange = (option) => {
     const value = multiple ? option.map(({ value }) => value) : option.value
     change(uid, value)
   }
 
-  const defaultValue = multiple
-    ? value.map((value) => {
-        const option = value.name || value
-        return { value: option, label: `${option.charAt(0)}${option.slice(1)}` }
-      })
-    : { label: `${value.charAt(0).toUpperCase()}${value.slice(1)}`, value }
+  const defaultValue = isEdit
+    ? multiple
+      ? value.map((value) => {
+          const option = value.name || value
+          return {
+            value: option,
+            label: `${option.charAt(0)}${option.slice(1)}`,
+          }
+        })
+      : { label: `${value.charAt(0).toUpperCase()}${value.slice(1)}`, value }
+    : null
 
   return (
     <>
